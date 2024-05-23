fba8298b183f9fb5371d6005440054a55f8d7708
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-05-23 15:07:11 +0530

    add shad cn uin theam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>Wlcome to pizza app</h1>
+      <Button >Test button 2</Button>
     </main>
   );
 }
