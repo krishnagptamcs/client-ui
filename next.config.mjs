945261e857dd04cf0bafeb269830232683758c7b
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-05-24 11:21:51 +0530

    new branch

--- next.config.mjs
+++ next.config.mjs
@@ -15,4 +15,5 @@ export default nextConfig;
 //Improving the overall user experience.
 //To ensure these optimizations are applied correctly, Next.js needs to know which external domains are allowed to serve images. This way, it can properly handle the images and apply the necessary transformations.
 
-//By requiring you to list trusted domains in the next.config.js file, Next.js ensures that your application benefits from enhanced security, better performance, and a more controlled and predictable behavior when dealing with external images. This explicit configuration approach aligns with best practices in modern web development, where security and performance are paramount.
+//By requiring you to list trusted domains in the next.config.js file, Next.js ensures that your application benefits from enhanced security, better performance, and a more controlled and predictable behavior when dealing with external images.
+// This explicit configuration approach aligns with best practices in modern web development, where security and performance are paramount.
